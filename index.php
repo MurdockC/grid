516b5cb3d25e7211d4db8433123a1c672066b464
Caleb <caleb@ramseymediaworks.com>
2018-01-16 16:56:33 -0600

    first commit '

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+  <title>Caleb Murdock</title>
+  <meta name="description" content="cool stuff I made">
+  <meta name="keywords" content="portfolio, Work, Caleb, Murdock, Graphic, Design, CSS, HTML, Web, Development">
+  <meta name="author" content="Caleb Murdock">
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+<link href="https://fonts.googleapis.com/css?family=PT+Mono" rel="stylesheet">
+</head>
+
+<body>
+  <div class="logo --jc--c --ai--c">
+    <img src="assets/images/dist/logo.png" alt="logo">
+  </div>
+  <div class="nav">
+    <div class="nav__item nav__Item1 --jc--c --ai--c"><span>About</span></div>
+    <div class="nav__item nav__Item2 --jc--c --ai--c"><span>Work</span></div>
+    <div class="nav__item nav__Item3 --jc--c --ai--c"><span>Contact</span></div>
+  </div>
+  <div class="main">
+    <div class="gallery">
+      <a href="#"><div class="gallery__Item gallery__Item1 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Dick<br>Lavy</div>
+      </div></a>
+      <a href="#"><div class="gallery__Item gallery__Item2 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Wintech</div>
+      </div></a>
+      <a href="#"><div class="gallery__Item gallery__Item3 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Doug<br>Andrus</div>
+      </div></a>
+      <a href="#"><div class="gallery__Item gallery__Item4 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Crete<br>Carrier</div>
+      </div></a>
+      <a href="#"><div class="gallery__Item gallery__Item5 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Five</div>
+      </div></a>
+      <a href="#"><div class="gallery__Item gallery__Item6 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Six</div>
+      </div></a>
+      <a href="#"><div class="gallery__Item gallery__Item7 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Seven</div>
+      </div></a>
+      <a href="#"><div class="gallery__Item gallery__Item8 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Eight</div>
+      </div></a>
+      <a href="#"><div class="gallery__Item gallery__Item9 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Nine</div>
+      </div></a>
+      <a href="#"> <div class="gallery__Item gallery__Item10 --jc--c --ai--c">
+        <img src="assets/images/dist/gallery_photo.jpg" alt="gallery photo one">
+        <div class="item__Title">Ten</div>
+      </div></a>
+    </div>
+  </div>
+</body>
---
+++ project_1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+  <title>Caleb Murdock</title>
+  <meta name="description" content="cool stuff I made">
+  <meta name="keywords" content="portfolio, Work, Caleb, Murdock, Graphic, Design, CSS, HTML, Web, Development">
+  <meta name="author" content="Caleb Murdock">
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+<link href="https://fonts.googleapis.com/css?family=PT+Mono" rel="stylesheet">
+</head>
+
+<body>
+  <div class="logo --jc--c --ai--c">
+    <img src="assets/images/dist/logo.png" alt="logo">
+  </div>
+  <div class="nav">
+    <div class="nav__item nav__Item1 --jc--c --ai--c"><span>About</span></div>
+    <div class="nav__item nav__Item2 --jc--c --ai--c"><span>Work</span></div>
+    <div class="nav__item nav__Item3 --jc--c --ai--c"><span>Contact</span></div>
+  </div>
+  <div class="main">
+
+    <div class="project">
+      <div class="project__Gallery">
+        <img class="project__Photo" src="assets/images/dist/gallery_photo.jpg" alt"Photo of Caleb">
+        <img class="project__Photo" src="assets/images/dist/gallery_photo.jpg" alt"Photo of Caleb">
+        <img class="project__Photo" src="assets/images/dist/gallery_photo.jpg" alt"Photo of Caleb">
+      </div>
+      <div class="project__Text">
+        <h3>Project One</h3>
+        <p>This is a landing page made for a company to hire drivers.</p>
+        <div class="project__buttons">
+          <a><button>Github</button></a>
+          <a><button>Visit</button></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
